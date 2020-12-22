6d103d75393db24dd6aaa83684b433ba616059ef
RaphaelQSmith <49982690+RaphaelQSmith@users.noreply.github.com>
2020-12-22 03:43:59 +0000

    "Header and footer"

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,8 +4,18 @@ export default class footer extends Component {
     render() {
         return (
             <div>
+                <footer class="page-footer teal lighten-2" style={{ textAlign: 'left' }}>
+                    <div class="footer-copyright">
+                        <div class="container">
+                            ©2020 Copyright - Raphael Smith
+                <a class="grey-text text-lighten-4 right" href="http://www.laundryonline.com/" target="false">Laundry
+                    Online</a>
+                        </div>
+                    </div>
+                </footer>
 
-            </div>
+            </div >
         )
     }
 }
+
--- src/components/header.js
+++ src/components/header.js
@@ -3,9 +3,39 @@ import React, { Component } from 'react'
 export default class header extends Component {
     render() {
         return (
-            <div>
-
-            </div>
+            <React.Fragment>
+                <nav className="nav-wrapper teal" style={navStyle}>
+                    <div className="container">
+                        <div className="row">
+                            <div>
+                                <div className="col s4">
+                                    <a href="/" className="brand-logo" style={titleStyle}>Roster System</a>
+                                </div>
+                                <div className="col s6">
+                                    <ul id="nav-mobile" class="right hide-on-med-and-down col-s4">
+                                        <li><a href="/home">Roster</a></li>
+                                        <li><a href="/newroster">Create</a></li>
+                                        <li><a href="/register">Register</a></li>
+                                        <li><a href="/logout">Logout</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </React.Fragment >
         )
     }
+
+}
+
+const navStyle = {
+    fontFamily: 'Quicksand',
+    fontWeight: 'bold',
+    display: 'flex',
+    textAlign: 'left'
+}
+const titleStyle = {
+    position: 'relative'
+
 }
