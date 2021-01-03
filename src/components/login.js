86e3a0eae11327e3d6e44240f579070594936c0e
RaphaelQSmith <49982690+RaphaelQSmith@users.noreply.github.com>
2021-01-02 22:56:30 -0300

    Handling login

--- src/components/login.js
+++ src/components/login.js
@@ -1,6 +1,51 @@
-import React, { PureComponent } from 'react'
+import React, { Component } from 'react'
+import axios from 'axios'
 
-export default class table extends PureComponent {
+
+export default class table extends Component {
+   constructor(props){
+       super(props)
+   
+   
+       this.state = {
+        email: '',
+        password: '',
+        loginErrors: ''
+        };
+        
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event){
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+   handleSubmit(event){
+    const { email, password} = this.state;
+    axios.post('http://localhost:3000/sessions', {
+        user:{
+            email: email,
+            password: password
+        }
+    },
+    {withCredentials: true}
+    )
+    .then(response => {
+        console.log('res from login', response)
+        
+        
+        // if(response.data.status === 'created'){
+        //     this.props.handleSuccessfulAuth(response.data)
+        // }
+    })
+    .catch(error=>{
+        console.log('Login not successful', error)
+    })
+    event.preventDefault()
+   }
+
     render() {
         return (
             <React.Fragment>
